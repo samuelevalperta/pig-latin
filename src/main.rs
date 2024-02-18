1d8946313c4253b4b6edc640ce3531e69fa8aaf0
Samuele Valperta <samuele.valperta@gmail.com>
2024-02-18 17:44:39 +0100

    Refactor is_vowel() in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,9 @@
 // Convert strings to pig latin. The first consonant of each word is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details about UTF-8 encoding!
 
 fn is_vowel(c: char) -> bool {
-    match c.to_ascii_lowercase() {
-        'a' | 'e' | 'i' | 'o' | 'u' => true,
-        _ => false,
-    }
+    matches!(c.to_ascii_lowercase(), 'a' | 'e' | 'i' | 'o' | 'u')
 }
+
 fn main() {
     let mut word = String::from("Здравствуйте");
 
@@ -19,7 +17,7 @@ fn main() {
                 result.push_str("-");
                 result.push(c);
                 result.push_str("ay");
-                word = result.clone();
+                word = result;
             }
         },
         None => println!("The string is empty"),
